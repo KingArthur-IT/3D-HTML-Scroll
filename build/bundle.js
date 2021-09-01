fc2552fb8190afa1f2196efd5900eb68993a52c8
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-09-01 20:35:35 +0300

    hils

--- build/bundle.js
+++ build/bundle.js
@@ -42827,10 +42827,11 @@
 	//scene
 	let canvas, camera, scene, light, renderer;
 	//for line
-	let posArray = [];
-	let lineMtl, lineGeometry, curve;
-
-
+	let posArrayLeft = [],
+		posArrayRight = [];
+	let railMtl, betweenRailMtl,
+		lineGeometryLeft, curveLeft,
+		lineGeometryRight, curveRight;
 	//params
 	let params = {
 		sceneWidth: 850,
@@ -42838,13 +42839,110 @@
 		bgColor: 0xaaaadd,
 		cameraProps: {
 			visibilityLength: 1000,
-			startPosition: new Vector3(0.0, 10.0, 1000.0)
+			startPosition: new Vector3(0.0, 10.0, 1000.0),
+			rotationAmplitude: 3.0
 		},
 		line: {
-			width: 10.0, //px
-			color: 0xffffff
-		}
+			width: 5.0, //px
+			color: 0xffffff,
+			forwardLength: 200,
+			sinAmplitude: 3.0,
+			sinPhase: 0.01
+		},
+		scrollStep: 0.1,
+		roadWidth: 2.0,
+		roadFrequency: 5
 	};
+
+	class Perlin {
+	    constructor() {
+	        this.grad3 =    
+	            [[1,1,0],[-1,1,0],[1,-1,0],[-1,-1,0], 
+	            [1,0,1],[-1,0,1],[1,0,-1],[-1,0,-1], 
+	            [0,1,1],[0,-1,1],[0,1,-1],[0,-1,-1]]; 
+	        this.p = [];
+	        for (var i=0; i<256; i++) {
+	            this.p[i] = Math.floor(Math.random()*256);
+	        }
+	        
+	        // To remove the need for index wrapping, double the permutation table length 
+	        this.perm = []; 
+	        for(i=0; i<512; i++) {
+	            this.perm[i]=this.p[i & 255];
+	        } 
+
+	        // A lookup table to traverse the simplex around a given point in 4D. 
+	        // Details can be found where this table is used, in the 4D noise method. 
+	        this.simplex = [ 
+	            [0,1,2,3],[0,1,3,2],[0,0,0,0],[0,2,3,1],[0,0,0,0],[0,0,0,0],[0,0,0,0],[1,2,3,0], 
+	            [0,2,1,3],[0,0,0,0],[0,3,1,2],[0,3,2,1],[0,0,0,0],[0,0,0,0],[0,0,0,0],[1,3,2,0], 
+	            [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0], 
+	            [1,2,0,3],[0,0,0,0],[1,3,0,2],[0,0,0,0],[0,0,0,0],[0,0,0,0],[2,3,0,1],[2,3,1,0], 
+	            [1,0,2,3],[1,0,3,2],[0,0,0,0],[0,0,0,0],[0,0,0,0],[2,0,3,1],[0,0,0,0],[2,1,3,0], 
+	            [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0], 
+	            [2,0,1,3],[0,0,0,0],[0,0,0,0],[0,0,0,0],[3,0,1,2],[3,0,2,1],[0,0,0,0],[3,1,2,0], 
+	            [2,1,0,3],[0,0,0,0],[0,0,0,0],[0,0,0,0],[3,1,0,2],[0,0,0,0],[3,2,0,1],[3,2,1,0]]; 
+	    }
+
+	    dot(g, x, y) { 
+	        return g[0]*x + g[1]*y;
+	    }
+
+	    noise(xin, yin) { 
+	        var n0, n1, n2; // Noise contributions from the three corners 
+	        // Skew the input space to determine which simplex cell we're in 
+	        var F2 = 0.5*(Math.sqrt(3.0)-1.0); 
+	        var s = (xin+yin)*F2; // Hairy factor for 2D 
+	        var i = Math.floor(xin+s); 
+	        var j = Math.floor(yin+s); 
+	        var G2 = (3.0-Math.sqrt(3.0))/6.0; 
+	        var t = (i+j)*G2; 
+	        var X0 = i-t; // Unskew the cell origin back to (x,y) space 
+	        var Y0 = j-t; 
+	        var x0 = xin-X0; // The x,y distances from the cell origin 
+	        var y0 = yin-Y0; 
+	        // For the 2D case, the simplex shape is an equilateral triangle. 
+	        // Determine which simplex we are in. 
+	        var i1, j1; // Offsets for second (middle) corner of simplex in (i,j) coords 
+	        if(x0>y0) {i1=1; j1=0;} // lower triangle, XY order: (0,0)->(1,0)->(1,1) 
+	        else {i1=0; j1=1;}      // upper triangle, YX order: (0,0)->(0,1)->(1,1) 
+	        // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and 
+	        // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where 
+	        // c = (3-sqrt(3))/6 
+	        var x1 = x0 - i1 + G2; // Offsets for middle corner in (x,y) unskewed coords 
+	        var y1 = y0 - j1 + G2; 
+	        var x2 = x0 - 1.0 + 2.0 * G2; // Offsets for last corner in (x,y) unskewed coords 
+	        var y2 = y0 - 1.0 + 2.0 * G2; 
+	        // Work out the hashed gradient indices of the three simplex corners 
+	        var ii = i & 255; 
+	        var jj = j & 255; 
+	        var gi0 = this.perm[ii+this.perm[jj]] % 12; 
+	        var gi1 = this.perm[ii+i1+this.perm[jj+j1]] % 12; 
+	        var gi2 = this.perm[ii+1+this.perm[jj+1]] % 12; 
+	        // Calculate the contribution from the three corners 
+	        var t0 = 0.5 - x0*x0-y0*y0; 
+	        if(t0<0) n0 = 0.0; 
+	        else { 
+	            t0 *= t0; 
+	            n0 = t0 * t0 * this.dot(this.grad3[gi0], x0, y0);  // (x,y) of grad3 used for 2D gradient 
+	        } 
+	        var t1 = 0.5 - x1*x1-y1*y1; 
+	        if(t1<0) n1 = 0.0; 
+	        else { 
+	            t1 *= t1; 
+	            n1 = t1 * t1 * this.dot(this.grad3[gi1], x1, y1); 
+	        }
+	        var t2 = 0.5 - x2*x2-y2*y2; 
+	        if(t2<0) n2 = 0.0; 
+	        else { 
+	            t2 *= t2; 
+	            n2 = t2 * t2 * this.dot(this.grad3[gi2], x2, y2); 
+	        } 
+	        // Add contributions from each corner to get the final noise value. 
+	        // The result is scaled to return values in the interval [-1,1]. 
+	        return 70.0 * (n0 + n1 + n2); 
+	    }
+	}
 
 	class App {
 		init() {
@@ -42874,34 +42972,73 @@
 			renderer.setClearColor(params.bgColor);
 			renderer.shadowMap.enabled = true;
 
-			const geometry = new BoxGeometry( 10, 10, 10);
+			const Boxgeometry = new BoxGeometry( 10, 10, 10);
 			const material = new MeshPhysicalMaterial( {color: 0xcccccc} );
-			const cube = new Mesh(geometry, material);
+			const cube = new Mesh(Boxgeometry, material);
 			cube.rotation.set(0.3, 0.3, 0.1);
 			cube.position.set(-10.0, params.cameraProps.startPosition.y, 0.0);
 			cube.receiveShadow = true;
 			scene.add(cube);
 			
 			//line
-			lineMtl = new LineMaterial({
+			railMtl = new LineMaterial({
 				color: params.line.color,
 				linewidth: params.line.width, 
 				resolution: new Vector2(params.sceneWidth, params.sceneHeight)
 			});
+			betweenRailMtl = new LineMaterial({
+				color: params.line.color,
+				linewidth: 2.0 * params.line.width, 
+				resolution: new Vector2(params.sceneWidth, params.sceneHeight)
+			});
 
-			lineGeometry = new LineGeometry();
+			lineGeometryLeft = new LineGeometry();
+			lineGeometryRight = new LineGeometry();
 			
 			for (let i = params.cameraProps.startPosition.z; i > 800; i--) {
-				let x = 3.0 * Math.sin(i * 0.05);
-				posArray.push(x, 0.0, i);
+				let x = params.line.sinAmplitude * Math.sin(i * params.line.sinPhase);
+				posArrayLeft.push(x, 0.0, i);
+				posArrayRight.push(x + params.roadWidth, 0.0, i);
+
+				if (i % params.roadFrequency == 0) {
+					const lineGeometry = new LineGeometry();
+					const pos = [x + 0.05, 0.0, i,
+						x + params.roadWidth - 0.05, 0.0, i];
+					lineGeometry.setPositions(pos);
+					const line = new Line2(lineGeometry, betweenRailMtl);
+					scene.add(line);
+				}
 			}		
-			lineGeometry.setPositions(posArray);
-			curve = new Line2(lineGeometry, lineMtl);
-			scene.add(curve);
+			lineGeometryLeft.setPositions(posArrayLeft);
+			lineGeometryRight.setPositions(posArrayRight);
+			curveLeft = new Line2(lineGeometryLeft, railMtl);
+			curveRight = new Line2(lineGeometryRight, railMtl);
+			scene.add(curveLeft);
+			scene.add(curveRight);
 
 			renderer.render(scene, camera);
 			canvas.addEventListener('mousemove', onMouseMove, false);
 			canvas.addEventListener('wheel', onScroll, false);
+
+			// Setup the terrain
+			var geometry = new PlaneGeometry( 2000, 2000, 256, 256 );
+			var mat = new MeshLambertMaterial({color: 0xcccccc});
+			var terrain = new Mesh( geometry, mat );
+			terrain.rotation.x = -Math.PI / 2;
+			scene.add( terrain );
+
+			var perlin = new Perlin();
+			var peak = 60;
+			var smoothing = 300;
+			var vertices = terrain.geometry.attributes.position.array;
+				for (var i = 0; i <= vertices.length; i += 3) {
+					vertices[i+2] = peak * perlin.noise(
+						(terrain.position.x + vertices[i])/smoothing, 
+						(terrain.position.z + vertices[i+1])/smoothing
+					);
+				}
+				terrain.geometry.attributes.position.needsUpdate = true;
+				terrain.geometry.computeVertexNormals();
 
 			animate();
 		}
@@ -42910,26 +43047,50 @@
 	function onMouseMove(e) {    
 	    let w = document.documentElement.clientWidth;
 	    let h = document.documentElement.clientHeight;
-	    let wk = 3.0 * (e.x - w * 0.5) / w;
-		let hk = 3.0 * (e.y - h * 0.5) / h;
+	    let wk = params.cameraProps.rotationAmplitude * (e.x - w * 0.5) / w;
+		let hk = params.cameraProps.rotationAmplitude * (e.y - h * 0.5) / h;
 		camera.position.x = wk;
 		camera.position.y = params.cameraProps.startPosition.y + hk;
 		camera.lookAt(0, params.cameraProps.startPosition.y,
 			camera.position.z - params.cameraProps.visibilityLength);
 	}
 	function onScroll(e) {
-		if (camera.position.z + e.deltaY * 0.1 < params.cameraProps.startPosition.z) {
-			camera.position.z += e.deltaY * 0.1;
-			scene.remove(curve);
-			lineGeometry = new LineGeometry();
+		if (camera.position.z + e.deltaY * params.scrollStep < params.cameraProps.startPosition.z) {
+			let step = 0.5 * e.deltaY * params.scrollStep;
+			let posMiddle = camera.position.z + step;
+			let posEnd = camera.position.z + 2.0 * step;
+
+			camera.position.z += 2.0 * step;
+
+			scene.remove(curveLeft);
+			scene.remove(curveRight);
+			lineGeometryLeft = new LineGeometry();
+			lineGeometryRight = new LineGeometry();
 			
-			let i = camera.position.z - 200;
-			let x = 3.0 * Math.sin(i * 0.05);
-			posArray.push(x, 0.0, i);
+			let zPosMiddle 	= posMiddle - params.line.forwardLength;
+			let zPosEnd 	= posEnd - params.line.forwardLength;
+			let xMiddle = params.line.sinAmplitude * Math.sin(zPosMiddle * params.line.sinPhase);
+			let xEnd = params.line.sinAmplitude * Math.sin(zPosEnd * params.line.sinPhase);
+			posArrayLeft.push(xMiddle, 0.0, zPosMiddle);
+			posArrayLeft.push(xEnd, 0.0, zPosEnd);
+			posArrayRight.push(xMiddle + params.roadWidth, 0.0, zPosMiddle);
+			posArrayRight.push(xEnd + params.roadWidth, 0.0, zPosEnd);
 			
-			lineGeometry.setPositions(posArray);
-			curve = new Line2(lineGeometry, lineMtl);
-			scene.add(curve);
+			lineGeometryLeft.setPositions(posArrayLeft);
+			lineGeometryRight.setPositions(posArrayRight);
+			curveLeft = new Line2(lineGeometryLeft, railMtl);
+			curveRight = new Line2(lineGeometryRight, railMtl);
+			scene.add(curveLeft);
+			scene.add(curveRight);
+
+			if (zPosEnd % params.roadFrequency == 0) {
+				const lineGeometry = new LineGeometry();
+				const pos = [xEnd + 0.05, 0.0, zPosEnd,
+					xEnd + params.roadWidth - 0.05, 0.0, zPosEnd];
+				lineGeometry.setPositions(pos);
+				const line = new Line2(lineGeometry, betweenRailMtl);
+				scene.add(line);
+			}
 		}
 	}
 
